44b20ca09c5db98269988b0c0b61701ee9052d1a
Вениамин Литвин <veniaminlitvin993@gmail.com>
2025-09-09 19:50:30 +0300

    lol

--- cw_09_09_2025/cw_09_09_2025.cpp
+++ cw_09_09_2025/cw_09_09_2025.cpp
@@ -50,7 +50,75 @@ void enter(St& entity) {
 
 }
 
-int main()
-{
-    
+void vedennya(St& entity) {
+    int choice = 0;
+    cout << "Yakiy tip?(1-bird, 2-hudoba, 3-ludina): ";
+    cin >> choice;
+    entity.t = Type(choice);
+    switch (entity.t) {
+    case Type::bird:
+        cout << "Yaka shvidkist polyotu: " << endl;
+        cin >> entity.h.bird_speed;
+        break;
+    case Type::animal:
+        cout << "vvedit ce parnokopitne chi ni? (1-tak, 0-ni) " << endl;
+        cin >> entity.h.yes_no;
+        break;
+    case Type::human:
+        cout << "Yake iq? " << endl;
+        cin >> entity.h.iq_human;
+        break;
+    default:
+        cout << "Error!" << endl;
+        break;
+    };
+    cout << "yakiy kolir: " << endl;
+    cin >> entity.color;
+    cout << "yaka shvidkist peresuvanna: " << endl;
+    cin >> entity.speed;
+};
+
+void Info(St& entity) {
+    cout << "Tip: ";
+    switch (entity.t) {
+    case Type::bird:
+        cout << "Bird\nShvidkist polyoty: " << entity.h.bird_speed << endl;
+        break;
+    case Type::animal:
+        cout << "Hudoba\nParnokopitne" << entity.h.yes_no << endl;
+        break;
+    case Type::human:
+        cout << "Ludina\nIq? " << entity.h.iq_human << endl;
+        break;
+    };
+    cout << "Color: " << entity.color << endl;
+    cout << "Speed: " << entity.speed << endl;
+    cout << endl;
+}
+
+int main() {
+    system("chcp 1251>null");
+
+    const int size = 10;
+    St array[size];
+    int rax = 0;
+
+    cout << "Введіть данні сущності: " << endl;
+    for (int i = 0; i < size; i++) {
+        int m = 0;
+        cout << "Чи будете вводити інформацію? (1-так, 2-ні): ";
+        cin >> m;
+        if (m == 2) break;
+        cout << i + 1 << ") ";
+        vedennya(array[i]);
+        rax++;
+    }
+
+    cout << "\nInfo po kozniy sushnosti: " << endl;
+    for (int i = 0; i < rax; i++) {
+        cout << "Sushinst #" << i + 1 << ") " << endl;
+        Info(array[i]);
+    }
+
+    return 0;
 }
